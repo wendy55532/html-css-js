253ea658f78b672a2397b57141a287bb51340199
wendy55532 <135014973+wendy55532@users.noreply.github.com>
2024-04-28 04:12:49 -0400

    Update scrip.js

--- scrip.js
+++ scrip.js
@@ -1,34 +1,46 @@
-const todoInput= document.querySelector('.input');
-const colletion=document.querySelector('.colletion');
-const todoform=document.querySelector('.todo-form');
+const todoInput = document.querySelector('.input');
+const collection = document.querySelector('.collection');
+const todoForm = document.querySelector('.todo-form');
+let todos = [];
 
-todoform.addEventListener('submit',(e) => {
+todoForm.addEventListener('submit', (e) => {
     e.preventDefault();
-    let todoValue=todoInput.value;
-    if (todoValue !=='');{
-    addToDOM(todoValue);
-    todoInput.value='';
-}
+    let todoValue = todoInput.value;
+    if (todoValue !== '') {
+        addToDOM(todoValue);
+        todos.push(todoValue);
+        storeTodoOnLocalStorage();
+        todoInput.value = '';
+    }
 });
-function addToDOM(value){
-    const li= document.createElement('li');
-    const todoTitle = document.createElement('spam');
-    const deleteButton= document.createElement('button');
 
-    todoTitle.innerText= value;
-    deleteButton=innerText= 'Borrar';
+function addToDOM(value) {
+    const li = document.createElement('li');
+    const todoTitle = document.createElement('span'); 
+    const deleteButton = document.createElement('button');
 
-    li.classList.add('.colletion-item');
+    todoTitle.innerText = value;
+    deleteButton.innerText = 'Borrar'; 
 
-    
+    li.classList.add('collection-item'); 
+
     deleteButton.addEventListener('click', (e) => {
-
+        
+        e.target.parentElement.remove();
+        
+        const index = todos.indexOf(value);
+        if (index !== -1) {
+            todos.splice(index, 1);
+            storeTodoOnLocalStorage();
+        }
     });
 
     li.appendChild(todoTitle);
     li.appendChild(deleteButton);
 
-    colletion.appendChild(li);
-
+    collection.appendChild(li);
 }
 
+function storeTodoOnLocalStorage() {
+    localStorage.setItem('todos', JSON.stringify(todos));
+}
